e220f16f43b993fb7a9220bc1e3446f9e85a0d68
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-10 01:02:52 +0200

    Create 05-max-number.cs

---
+++ simple-loops/05-max-number.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Loops_Exercises
+{
+    class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > max)
+                    max = num;
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
