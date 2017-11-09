306d426771d190d5fc1521bdb6c1d19aabd90d86
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-10 00:15:17 +0200

    Create 04-triangle-of-55-stars.cs

---
+++ first-steps-in-coding/04-triangle-of-55-stars.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("*****");
+            Console.WriteLine("******");
+            Console.WriteLine("*******");
+            Console.WriteLine("********");
+            Console.WriteLine("*********");
+            Console.WriteLine("**********");
+
+
+        }
+    }
+}
