01d693716021490b05750dc29ad72cf819423fea
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-10 00:59:18 +0200

    Create 10-animal-type.cs

---
+++ complex-conditional-statements/10-animal-type.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Conditionals_Exercises
+{
+    class AnimalType
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            switch (animal)
+            {
+                case "dog": Console.WriteLine("mammal"); break;
+                case "crocodile": Console.WriteLine("reptile"); break;
+                case "tortoise": Console.WriteLine("reptile"); break;
+                case "snake": Console.WriteLine("reptile"); break;
+                default: Console.WriteLine("unknown"); break;
+            }
+        }
+    }
+}
