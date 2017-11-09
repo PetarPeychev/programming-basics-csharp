ad11bb727d5c14145ac8769633ff03eab34d537f
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-10 00:22:50 +0200

    Create 08-triangle-area.cs

---
+++ simple-calculations/08-triangle-area.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = Math.Round(a * h / 2, 2);
+            Console.WriteLine("Triangle area = {0}",area);
+        }
+    }
+}
